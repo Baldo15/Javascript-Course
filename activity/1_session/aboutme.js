e9e753524c10783297c9b18ec67d3af57c17756d
baldomeroOsorio <bosorio1117@gmail.com>
2023-05-10 17:51:03 -0500

    Se realiza la actividad uno y se documenta en un archivo

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,10 @@
-console.log("hello world");
+// Definimos las tres variables
+const nombre_apellido = "Baldomero Osorio Carmona";
+const titulo = "Ingeniero de sistemas";
+const cargo_deseado = "desarrollador de software senior";
 
+// Adicionamos todo en una sola variable
+const presentacion = `Hola, me llamo ${nombre_apellido} y me dedico a ${titulo}. Estoy cursando este Máster porque me gustaria trabajar en ${cargo_deseado}.`;
 
-console.log("laater");
+// Imprimimos la cadena en la consola
+console.log(presentacion);
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,42 @@
-// Ejercicio 1
-import "./aboutme.js";
-console.log("Ejercicio 1");
+// // Ejercicio 1
+// // Importamos el archivo aboutme.js
+// const presentacion = require('./aboutme');
 
-// Ejercicio 2
+// // Ejercicio 1
+// console.log("Ejercicio 1");
 
-// Ejercicio 3
+// // Ejercicio 2
 
-// Ejercicio 4
+// // Ejercicio 3
 
-// Ejercicio 5
+// // Ejercicio 4
+// console.log("los numeros divisibles por 7 menores que 100 son:")
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 7 === 0) {
+//       console.log(i);
+//     }
+//   }
+// // Ejercicio 5
 
 // Ejercicio 6
+// se crea el objeto
+const Formatter = function(prefix) {
+    this.prefix = prefix;
+  };
+  
+  Formatter.prototype.append = function(message) {
+    console.log(this.prefix + message);
+  };
+  
+  // se añade el metodo
+  Formatter.prototype.toLowerString = function(message) {
+    console.log(message.toLowerCase());
+  };
+  
 
+  const formatter = new Formatter("Hello ");
+  
+  // se llaman los metodos y se le asignan los mensajes
+  formatter.append("World"); 
+  formatter.toLowerString("I'm Lucas"); 
+  
